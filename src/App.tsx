e971b174b2975ca9021a497b71f387baf1806db1
maksim <krovorgen@gmail.com>
2022-02-04 18:33:36 +0300

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Registr } from './components/Registr';
 import { Login } from './components/Login';
 import { Main } from './components/Main';
 import { ToastContainer } from 'react-toastify';
+import { Error404 } from './components/Error404';
 
 export const App = () => {
   return (
@@ -12,10 +13,12 @@ export const App = () => {
       <Link to="/">Main</Link>
       <Link to="/login">Login</Link>
       <Link to="/registration">Registr</Link>
+      <Link to="/404">Error404</Link>
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
         <Route path="/registration" element={<Registr />} />
+        <Route path="/404" element={<Error404 />} />
       </Routes>
       <ToastContainer
         position="bottom-left"
