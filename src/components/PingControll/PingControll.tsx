214445bebac0fac30f572879451458c9a6c1a192
maksim <krovorgen@gmail.com>
2022-02-05 20:27:26 +0300

    #1 Стилизация отображения

--- src/components/PingControll/PingControll.tsx
+++ src/components/PingControll/PingControll.tsx
@@ -1,14 +1,28 @@
 import React, { useEffect, useState } from 'react';
+import cn from 'classnames';
 
 import { api } from '../../api';
 
 import styles from './PingControl.module.scss';
 
 export const PingControl = () => {
-  const [ping, setPing] = useState('');
+  const [ping, setPing] = useState(0);
 
   useEffect(() => {
-    api.checkPing(Date.now()).then(({ data }) => setPing(data.ping));
+    api.checkPing(Date.now()).then(({ data }) => setPing(+data.ping));
   }, []);
-  return <div>Ping: {ping}</div>;
+  return (
+    <p className={styles.root}>
+      Ping:{' '}
+      <mark
+        className={cn(styles.ping, {
+          [styles.low]: ping >= 0 && ping <= 33,
+          [styles.medium]: ping >= 34 && ping <= 66,
+          [styles.high]: ping >= 67,
+        })}
+      >
+        {ping}
+      </mark>
+    </p>
+  );
 };
