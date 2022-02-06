6776791026b45d80ec3c2c75df05ef71e09128d2
Artiom <artiomb5@yandex.by>
2022-02-06 11:19:20 +0300

    1. Added basic registration functionality without password checking. 2. Added the mixin with flex column formatting; 3. Added a few styles for the registration component.

--- src/pages/Registr/Registr.tsx
+++ src/pages/Registr/Registr.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import cn from 'classnames';
 
 import { Input } from '@alfalab/core-components/input';
@@ -9,41 +9,105 @@ import { Row } from '@alfalab/core-components/grid/row';
 import { Col } from '@alfalab/core-components/grid/col';
 
 import styles from './Registr.module.scss';
+import { api } from '../../api';
 
 export const Registr = () => {
-  const [passwordVisible, setPasswordVisible] = React.useState(false);
+  const [passwordsVisible, setPasswordsVisible] = React.useState({
+    password: false,
+    confirmPassword: false,
+  });
+  const [inputsData, setInputsData] = React.useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const visibilityHandler = (visible: boolean, name: 'password' | 'confirmPassword') => {
+    switch (name) {
+      case 'password':
+        setPasswordsVisible({ ...passwordsVisible, password: visible });
+        break;
+      case 'confirmPassword':
+        setPasswordsVisible({ ...passwordsVisible, confirmPassword: visible });
+        break;
+      default:
+        break;
+    }
+  };
+
+  const inputsChangeHandler = (event: ChangeEvent<HTMLInputElement>, payload: { value: string }) => {
+    console.log(event.currentTarget.name);
+    console.log(event.currentTarget.value);
+    switch (event.currentTarget.name) {
+      case 'email':
+        setInputsData({ ...inputsData, email: event.currentTarget.value });
+        break;
+      case 'password':
+        setInputsData({ ...inputsData, password: event.currentTarget.value });
+        break;
+      case 'confirmPassword':
+        setInputsData({ ...inputsData, confirmPassword: event.currentTarget.value });
+        break;
+      default:
+        break;
+    }
+  };
+
+  const registerHandler = () => {
+    api.register(inputsData.email, inputsData.password).then((data) => console.log(data.data));
+  };
 
   const test = () => {
     return (
-      <div>
+      <div className={styles.window}>
         <div>
           <Typography.Title tag={'h1'}>It-incubator</Typography.Title>
           <Typography.Title tag={'h2'}>Sign Up</Typography.Title>
         </div>
         <div>
-          <Input label="Email" name="email" />
-          <br />
-          <PasswordInput
-            passwordVisible={passwordVisible}
-            onPasswordVisibleChange={(visible) => {
-              setPasswordVisible(visible);
-            }}
+          <div>Email:</div>
+          <Input
+            name="email"
+            value={inputsData.email}
+            onChange={inputsChangeHandler}
+            type={'email'}
+            className={styles.input}
           />
           <br />
+          {/*password input field*/}
+          <div>Password:</div>
           <PasswordInput
-            passwordVisible={passwordVisible}
+            passwordVisible={passwordsVisible.password}
             onPasswordVisibleChange={(visible) => {
-              setPasswordVisible(visible);
+              visibilityHandler(visible, 'password');
             }}
+            value={inputsData.password}
+            name="password"
+            onChange={inputsChangeHandler}
+          />
+          <br />
+          {/*password confirm field*/}
+          <div>Confirm password:</div>
+          <PasswordInput
+            passwordVisible={passwordsVisible.confirmPassword}
+            onPasswordVisibleChange={(visible) => {
+              visibilityHandler(visible, 'confirmPassword');
+            }}
+            value={inputsData.confirmPassword}
+            name="confirmPassword"
+            onChange={inputsChangeHandler}
           />
         </div>
+        {/*buttons*/}
         <div>
           <Row align="middle">
             <Col>
               <Button view="secondary">Cancel</Button>
             </Col>
             <Col>
-              <Button view="primary">Register</Button>
+              <Button view="primary" onClick={registerHandler}>
+                Register
+              </Button>
             </Col>
           </Row>
         </div>
