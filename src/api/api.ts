5a13d9fa01eaca3e9a1d8035f3c617f83da86a64
LiubovSchmit <liubou.schmit@gmail.com>
2022-02-07 13:42:11 +0100

    #5 api logout and type created, instance corrected

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,14 +1,27 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
 const BASE_URL =
   process.env.NODE_ENV === 'development' ? 'http://localhost:7542/2.0/' : 'https://neko-back.herokuapp.com/2.0/';
 
 const instance = axios.create({
+  withCredentials: true,
   baseURL: BASE_URL,
+  headers: {},
 });
 
 export const api = {
   checkPing(frontTime: number) {
     return instance.post(`ping`, { frontTime });
   },
+};
+
+export const logoutApi = {
+  LogOut() {
+    return instance.delete<{}, AxiosResponse<InfoResponseType>>(`auth/me`);
+  },
+};
+
+export type InfoResponseType = {
+  info: string;
+  error: string;
 };
