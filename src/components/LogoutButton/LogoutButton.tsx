724ac0901f04c25c2fc7696c8011ace65cba0543
ArtiomB5 <artiomb5@yandex.by>
2022-02-13 12:18:37 +0300

    #2 Reverse

--- src/components/LogoutButton/LogoutButton.tsx
+++ src/components/LogoutButton/LogoutButton.tsx
@@ -8,7 +8,6 @@ import { api } from '../../api';
 import { catchHandler } from '../../helpers/catchHandler';
 
 import styles from './LogoutButton.module.scss';
-import { clearUserAccountData } from '../../redux/reducer/userAccount-reducer';
 
 export const LogoutButton = () => {
   const dispatch = useAppDispatch();
@@ -19,7 +18,6 @@ export const LogoutButton = () => {
       .then(({ data }) => {
         toast.success(data.info);
         dispatch(setIsLoggedIn(false));
-        dispatch(clearUserAccountData());
       })
       .catch(catchHandler);
   }, [dispatch]);
--- src/redux/thunk/login-thunk.ts
+++ src/redux/thunk/login-thunk.ts
@@ -3,16 +3,14 @@ import { api } from '../../api';
 import { setIsLoggedIn, setLoginLoading } from '../reducer/login-reducer';
 import { toast } from 'react-toastify';
 import { catchHandler } from '../../helpers/catchHandler';
-import { setUserAccountData } from '../reducer/userAccount-reducer';
 
 export const loginTC = (email: string, password: string, rememberMe: boolean) => (dispatch: AppDispatch) => {
   dispatch(setLoginLoading(true));
 
   api
     .login(email, password, rememberMe)
-    .then((response) => {
+    .then(() => {
       dispatch(setIsLoggedIn(true));
-      dispatch(setUserAccountData({ _id: response.data._id, name: response.data.name }));
       toast.success('You have successfully logged in');
     })
     .catch(catchHandler)
